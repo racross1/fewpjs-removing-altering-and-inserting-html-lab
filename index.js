54c9b78b2e3200f3355370eac3ff29c99e5f2117
racross1 <racross1@gmail.com>
2021-01-12 10:40:15 -0800

    commit tree 0dbbceb7c579a799f729913731540d74176540fe

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+let main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.head.appendChild(newHeader)
+
+newHeader.innerHTML = 'Rachel is the champion'
+
